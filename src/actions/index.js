f58fee0f65f94dc495a003c4fb5f36ab32aceade
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-11-21 00:09:37 -0500

    created one thunk action and two standard actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,30 @@ export const FETCH_SMURFS_FAIL = 'FETCH_SMURFS_FAIL';
 export const ADD_SMURF = 'ADD_SMURF';
 export const ADD_ERROR_MESSAGE = 'ADD_ERROR_MESSAGE';
 
+export const fetchSmurfs = () => {
+    return dispatch => {
+        dispatch({ type: FETCH_SMURFS_LOADING });
+        axios 
+            .get('http://localhost:3333/smurfs')
+            .then(res => {
+                console.log("response", res);
+                dispatch({ type: FETCH_SMURFS_SUCCESS, payload: res.data });
+            })
+            .catch(err => {
+                console.log("error", err);
+                dispatch({ type: FETCH_SMURFS_FAIL, payload: err.error })
+            })
+    }
+}
+
+export const addSmurf = (newSmurfId) => {
+    return { type: ADD_SMURF, payload: newSmurfId };
+}
+
+export const addError = (error) => {
+    return { type: ADD_ERROR_MESSAGE, payload: error };
+}
+
 //Task List:
 //1. Add a thunk action called fetchSmurfs that triggers a loading status display in our application, performs an axios call to retreive smurfs from our server, saves the result of that call to our state and shows an error if one is made.
 //2. Add a standard action that allows us to add new smurf (including the name, nickname, position, summary)
